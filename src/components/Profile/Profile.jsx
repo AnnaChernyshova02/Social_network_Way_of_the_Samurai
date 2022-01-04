fc6b15bce2cef379033e9add7e21f81426baa66e
Anna Chernyshova <ann020894@gmail.com>
2022-01-04 18:37:20 +0300

    lesson 21,22,23

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Profile() {
     return <div>
-        <div>
-            <img
-                src={'https://s.zagranitsa.com/images/articles/6729/870x486/53d189dfcd54fa9ecae756ddf5a7c2ee.jpg?1530714543'}/>
-        </div>
-        <div>
-            ava + description
-        </div>
+        <ProfileInfo/>
         <MyPosts/>
     </div>
 }
