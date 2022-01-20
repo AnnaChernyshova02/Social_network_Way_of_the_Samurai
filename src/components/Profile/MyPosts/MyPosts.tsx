aea9d0f10f711569a1762bc9f485b83dfacfde0f
Lafko <Bylafko@gmail.com>
2022-01-20 19:17:16 +0300

    lesson 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {createRef, useRef} from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {PostPropsType} from "./Post/Post"
@@ -12,15 +12,22 @@ function MyPosts({posts}: MyPostsPropsType) {
 
     let postsElement = posts.map( p => <Post message={p.message} likeCounts={p.likeCounts} />)
 
+    let newPostElement = createRef<HTMLTextAreaElement>();
+
+    let addPost = () => {
+        let text = newPostElement.current?.value;
+        alert(text)
+    }
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement} />
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
