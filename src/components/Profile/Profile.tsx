3f2d988d53caef7f5f8262a489c390132cc58aa1
Anna Chernyshova <ann020894@gmail.com>
2022-01-25 12:01:16 +0300

    lesson 32,33

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {PostPropsType} from "./MyPosts/Post/Post";
+import {addPost, ProfilePropsType} from "../../Redux/State";
 
-type ProfilePropsType = {
-    posts: Array<PostPropsType>
-}
-
-function Profile({posts}:ProfilePropsType) {
-
+function Profile({posts}: ProfilePropsType) {
 
     return <div>
         <ProfileInfo/>
-        <MyPosts posts={posts}/>
+        <MyPosts posts={posts} addPost={addPost}/>
     </div>
 }
 
